5edf300ffe40d5b9ddeeb1f8a01c15ad94065625
MarineDeveza <marine.deveza@gmail.com>
2023-03-28 18:04:17 +0200

    cardsv3

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import PokemonCard from './components/PokemonCard'
+
+import PokemonCard from "./PokemonCard";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,10 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <figure>
-         <img src="my-new-app/src/assets/bulbasaur.png" alt="Bulbasaur"/>
-         <figcaption>Bulbasaur</figcaption>
-      </figure>
+      <div>
+      <PokemonCard />
+      </div>
     </div>
   )
 }
